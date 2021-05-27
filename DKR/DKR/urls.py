219a180a8fd8aa1eb4a02883d3040064be6aab08
Illia Oleksenko <iliaoob2@gmail.com>
2021-05-27 18:58:05 +0300

    DKR: Added docker

--- DKR/DKR/urls.py
+++ DKR/DKR/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
-from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
 from notes import views
 
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
     path('users/', include('users.urls')), 
     path('users/', include('django.contrib.auth.urls')),
     url(r'^login/$', LoginView.as_view(), name='login'),
--- DKR/notes/tests.py
+++ DKR/notes/tests.py
@@ -1,3 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from users.models import CustomUser
 
-# Create your tests here.
+
+class TestingAuth(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.list_url = reverse('note_list')
+        self.create_url = reverse('note_new')
+        self.signup_url = reverse('login')
+        self.login_url = reverse('signup')
+        self.user = CustomUser.objects.create_user('test', 'test@gmail.com', 'password')
+        self.user.save()
+
+    def test_access_signup_page(self):
+        response = self.client.get(self.signup_url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_access_login_page(self):
+        response = self.client.get(self.login_url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_login(self):
+
+        user = CustomUser.objects.filter(username='test').first()
+        user.is_active=True
+        user.save()
+        data = {
+            'username':'test',
+            'email':'test@gmail.com',
+            'password':'password'
+        }
+        response = self.client.post(self.login_url, data, format='text\html')
+        self.assertEquals(response.status_code, 200)
+
