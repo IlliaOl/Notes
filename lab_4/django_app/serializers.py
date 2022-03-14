663d94cf974f3c88c192e29a45372d40c7824d9d
Illia Oleksenko <77388859+IlliaOl@users.noreply.github.com>
2022-03-14 19:27:37 +0200

    Delete lab_4 directory

--- lab_4/django_app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import BlackListModel
-
-
-class BlackListSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(required=False)
-    class Meta:
-        model = BlackListModel
-        fields = '__all__'
