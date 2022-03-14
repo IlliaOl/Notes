fb9e2020f8f7675368f8b7d4eeddfdc404512b35
Illia Oleksenko <77388859+IlliaOl@users.noreply.github.com>
2022-03-14 19:27:25 +0200

    Delete lab_3 directory

--- lab_3/lab_3/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from django_app.views import CalculateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('calculate/', CalculateView.as_view()),
-]
